a56c27a3e705a01ac98a27165da73d497839b21a
Lucas Maciel <03lucasmaciel@gmail.com>
2023-06-01 11:41:19 +0100

    feat add : modal

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var card = document.getElementById("card");
+    var modal = document.getElementById("modal");
+
+    card.addEventListener('click', function () {
+        // Exibe o modal, alterando o estilo do CSS
+        modal.style.display = 'block';
+    });
+
+    // when click on modal, close it
+    modal.addEventListener('click', function () {
+        modal.style.display = 'none';
+    }
+    );
+});
+
+
+
+
