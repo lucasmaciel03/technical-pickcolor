8c4a0b8a8450d6ea6d070355ef3aaa6287df86ae
AndreMaia98 <andre.maia@jcanao.pt>
2023-06-01 14:28:43 +0100

    fix: style, position of svg's

--- js/script.js
+++ js/script.js
@@ -1,19 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var card = document.getElementById("card");
-    var modal = document.getElementById("modal");
+document.addEventListener("DOMContentLoaded", function () {
+  var card = document.getElementById("card");
+  var modal = document.getElementById("modal");
 
-    card.addEventListener('click', function () {
-        // Exibe o modal, alterando o estilo do CSS
-        modal.style.display = 'block';
-    });
+  card.addEventListener("click", function () {
+    // Exibe o modal, alterando o estilo do CSS
+    modal.style.display = "block";
+  });
 
-    // when click on modal, close it
-    modal.addEventListener('click', function () {
-        modal.style.display = 'none';
-    }
-    );
+  // when click on modal, close it
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 });
-
-
-
-
